7537350f8884f2cda3863f1683e8cce9bfcb8b04
riasadmahbub <riasadbmahbub@gmail.com>
2021-03-21 13:00:31 -0500

    change in syntax

--- write_pinnacle_slurm.py
+++ write_pinnacle_slurm.py
@@ -11,23 +11,34 @@ number_of_processors = 1
 wall = 1 #this is in hours
 
 # This section prints the header/required info for the PBS script
-print('#PBS -N', job_name) # job name
-print('#PBS -q', queue) # which queue to use
-print('#PBS -j oe') #join the STDOUT and STDERR into a single file
-print('#PBS -o', job_name, '.$PBS_JOBID') # set the name of the job output file
-print('#PBS -l nodes=' + str(number_of_nodes)+':ppn='+str(number_of_processors)) # how many resource to ask for (nodes = num nodes, ppn = num processors)
-print('#PBS -l walltime=' + str(wall) + ':00:00') # set the walltime (default to 1 hr)
+print("#SBATCH -J", job_name) # job name
+print("#SBATCH --partition", queue)  # which queue to use
+print("#SBATCH -oe") #join the STDOUT and STDERR into a single file
+print("#SBATCH --nodes=" + str(number_of_nodes)) # how many nodes
+print("#SBATCH --ntasks-per-node=" + str(number_of_processors)) #how many processors
+print("#SBATCH --time=" + str(wall)) # set the walltime (default to 1 hr)
 
-# cd into working directory
-print('cd $PBS_O_WORKDIR')
-print()
+print("export OMP_NUM_THREADS=32")
+ 
+# load required modules
+print('module load samtools')
+print('module load jellyfish')
+print('module load bowtie2')
+print('module load salmon/0.8.2')
+print('module load java')
+ 
+# cd into the directory where you're submitting this script from
+print("cd $SLURM_SUBMIT_DIR")
 
-#load the necessary modules
-print('# load modules')
-print('module purge')
-print('module load gcc/7.2.1')
-print()
+# copy files from storage to scratch
+print('rsync -av RNA-R*.fastq.gz /scratch/$SLURM_JOB_ID')
 
-# commands for this job
-print('# insert commands here')
+# cd onto the scratch disk to run the job
+print("cd /scratch/$SLURM_JOB_ID/")
+
+# run the Trinity assembly
+print("/share/apps/bioinformatics/trinity/trinityrnaseq-v2.11.0/Trinity --seqType fq --left RNA-R1.fastq.gz --right RNA-R2.fastq.gz --CPU 48 --max_memory 250G --trimmomatic --no_normalize_reads --full_cleanup --output trinity_Run2")
+ 
+# copy output files back to storage
+print("rsync -av trinity_Run2 $SLURM_SUBMIT_DIR")
 
